1368305dea5374948b039aeae83bb7ffcf4547fb
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-06-27 18:41:59 +0600

    implement betterauth

---
+++ src/lib/auth.js
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("epiloguelab-db");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, { client }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+});
